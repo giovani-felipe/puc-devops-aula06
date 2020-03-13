167c42226ea85f46f351811ab5381541f1b715b9
Bruno Vasconcellos Augusto <478786@sga.pucminas.br>
2020-03-12 22:04:57 -0300

    Configure jest

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,5 @@ const {defaults} = require('jest-config');
 
 module.exports = {
   verbose: true,
-  testEnvironment: 'node',
-  "roots": [
-    "<rootDir>/src"
-  ],
-  testMatch: [
-    "**/__tests__/**/*.+(js)",
-    "**/?(*.)+(spec|test).+(js)"
-  ],
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
+  testEnvironment: 'node'
 };
